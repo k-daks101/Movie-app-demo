63d8d082988a1b63d7f2e555d7bf28e8c0d3b0de
Kwame Dako <147068894+k-daks101@users.noreply.github.com>
2025-07-20 19:52:29 +0100

    trying to console log the problem

--- JS/script105.js
+++ JS/script105.js
@@ -143,10 +143,21 @@ async function displayPopularMovies() {
 // -----------------------------
 // Popular Shows
 // -----------------------------
+function getPoster(posterPath) {
+  return posterPath 
+    ? `https://image.tmdb.org/t/p/w500${posterPath}` 
+    : 'images/no-image.jpg';
+}
+
 async function displayPopularShows() {
   const { results } = await fetchAPIData('tv/popular');
   const container = document.querySelector('#popular-shows');
   if (!container) return;
+
+  console.log(results);
+  results.forEach((show) => {
+    console.log(show.name, show.poster_path); // helps debug
+  });
 
   container.innerHTML = '';
   results.forEach((show) => {
